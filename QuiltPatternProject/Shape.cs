11561b156dcf3dbc3dd8f5613d05c468799c9e3e
Syed Malik <ayazsah@gmail.com>
2021-09-04 13:08:12 -0400

    Add Shape Class

---
+++ QuiltPatternProject/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuiltPatternProject
+{
+    public abstract class Shape
+    {
+        string name = "Shape Name";
+        string color = "Shape Color";
+
+        public decimal GetArea(decimal area)
+        {
+            return area;
+        }
+    }
+}
