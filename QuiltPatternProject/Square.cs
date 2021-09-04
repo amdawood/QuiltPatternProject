1bc1d9dcd87ab1acd5354ed4893312ddcb5c7fcc
Syed Malik <ayazsah@gmail.com>
2021-09-04 13:18:45 -0400

    Add Square Class

---
+++ QuiltPatternProject/Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuiltPatternProject
+{
+    public class Square : Shape
+    {
+        public decimal side1;
+        private string Name;
+
+        public string Color { get; private set; }
+        public decimal sie1 { get; private set; }
+
+        public Square(string color, decimal side)
+        {
+            Name = "Square";
+            Color = color;
+            side1 = side;
+        }
+    }
+}
